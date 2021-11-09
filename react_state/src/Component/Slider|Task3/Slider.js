39d224fd33765616ee423973b97268932d88a8b4
Anasteisha Shamrelyuk <anasteisha@Anasteishas-MacBook-Air.local>
2021-11-09 19:02:32 +0200

    fix

--- react_state/src/Component/Slider|Task3/Slider.js
+++ react_state/src/Component/Slider|Task3/Slider.js
@@ -1,11 +1,7 @@
 import { useState } from "react";
 import { FaGreaterThan } from 'react-icons/fa';
 import { FaLessThan } from 'react-icons/fa';
-
-let style={
-    height: '300px',
-    display: 'none',
-}
+import '../../App.css';
 
 const slides = [
     {
@@ -21,37 +17,36 @@ const slides = [
 ];
 
 function Slider() {
-    let [slide, setSlider] = useState(1)
+	let [slide, setSlider] = useState(1)
+	function useSetSliderRight() {
+			(slide <= 1) ? setSlider(slide = 2) : setSlider(slide - 1)
+	}
+	function useSetSliderLeft() {
+			(slide > slides.length-1) ? setSlider(slide = 1) : setSlider(slide + 1)
+	}
 
-        const link = {
-            [true]: 'https://www.befunky.com/images/wp/wp-2014-08-milky-way-1023340_1280.jpg?auto=webp&format=jpg&width=1136&crop=16:9',
-            [false]: 'https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg'
-        }
+	const shownSlide = slides.map(item =>{
+		(item.id == slide) ? 
+		item.className = "imgSliderBlock" : 
+		item.className = "imgSliderNone";
+		console.log(slide)
+		return <img id={item.id} className={item.className} src={item.img} alt={item.text}/> 
+		} 
+	)
 
-        function changeOnRight() {
-            (slide <= 1) ? setSlider(slide = 2) : setSlider(slide - 1)
-        }
-        function changeOnLeft() {
-            (slide > slides.length-1) ? setSlider(slide = 1) : setSlider(slide + 1)
-        }
-
-    return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <div>
-                <FaLessThan onClick={changeOnRight}/>
-            </div>
-            <div> 
-                {slides.map( item =>{
-                    (item.id == slide)? style = {display: 'block', height: '300px'} : style = {display: 'none', height: '300px'};
-                    return( <img id={item.id} style={style} src={item.img} alt={item.text}  /> )
-                } )}
-            </div>
-            <div>
-                <FaGreaterThan onClick={changeOnLeft} />
-            </div>
-        </div>
-    )
-
+	return (
+		<div className = 'sliderWrapper'>
+			<div>
+					<FaLessThan onClick={useSetSliderRight}/>
+			</div>
+			<div> 
+					{shownSlide}
+			</div>
+			<div>
+					<FaGreaterThan onClick={useSetSliderLeft}/>
+			</div>
+		</div>
+	)
 }
 
 export default Slider
