0ae0ff481cb5114033a3c0acfc34513e033d682d
Anasteisha Shamrelyuk <anasteisha@Anasteishas-MacBook-Air.local>
2021-11-10 15:50:27 +0200

    fix

---
+++ react_state/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import {ShowButton} from './Component/Button|Task2';
+
+function App() {
+  return (
+    <div className="App">
+      <ShowButton/>
+    </div>
+  );
+}
+
+export default App;
--- react_state/src/Component/Button|Task2/ShowButton.js
+++ react_state/src/Component/Button|Task2/ShowButton.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 
 function ShowButton() {
